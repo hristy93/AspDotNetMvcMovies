901787bfaba28b30769e2d20f41c0a8c0b5735e1
hristy93 <hristy93@gmail.com>
2017-10-18 15:01:52 +0300

    The initial commit of the ASP.NET MVC Movies project

---
+++ AspDotNetMvcMovies/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspDotNetMvcMovies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is my <b>first page</b> ...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
