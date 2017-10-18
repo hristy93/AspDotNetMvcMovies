5b843975e6e8300bfd7e616cb00e05216e421fe2
hristy93 <hristy93@gmail.com>
2017-10-18 17:34:19 +0300

    Added some attributes for the properties of the Movie model

--- AspDotNetMvcMovies/Models/Movie.cs
+++ AspDotNetMvcMovies/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,22 @@ namespace AspDotNetMvcMovies.Models
 {
     public class Movie
     {
+        private enum GenreType
+        {
+            Action,
+            Drama,
+            ScienceFiction,
+        }
+
         public int ID { get; set; }
         public string Title { get; set; }
+        [DisplayName("Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+        [EnumDataType(typeof(GenreType), ErrorMessage = "Choose from Action, Drama or ScienceFiction")]
         public string Genre { get; set; }
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 }
